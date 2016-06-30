c55cbec48c95e3dbd4300cf8de7339f795a03122
Bhupendra Piprava <bdpiparva@gmail.com>
2016-06-30 16:16:36 +0530

    create LibraryManagment class and abstract all static methods

---
+++ src/main/LibraryManagment.java
@@ -0,0 +1,62 @@
+package main;
+
+public class LibraryManagment {
+
+
+    public  boolean loggedIn = false;
+
+
+
+
+    public boolean loggedIn() {
+        return loggedIn;
+    }
+
+
+    public  void clearLogin() {
+        loggedIn = false;
+//
+    }
+
+
+    public void showMovieList() {
+        System.out.println(createMovie("Rocky", "John G. Avildsen", "10"));
+        System.out.println(createMovie("Rocky II", "John G. Avildsen", "9"));
+        System.out.println(createMovie("Rocky III", "John G. Avildsen", "8"));
+        System.out.println(createMovie("Rocky IV", "John G. Avildsen", "7"));
+        System.out.println(createMovie("Rocky V", "John G. Avildsen", "8"));
+        System.out.println(createMovie("Drainage", "Francisco Trindade", "N/A"));
+        System.out.println(createMovie("The Shawshank Redemption", "Frank Darabont", "10"));
+        System.out.println(createMovie("The Godfather", "Francis Ford Coppola", "7"));
+        System.out.println(createMovie("Inception", "Frank Darabont", "10"));
+        System.out.println(createMovie("Pulp Fiction", "Quentin Tarantino", "6"));
+    }
+
+    private String createMovie(String movieTitle, String movieDirector, String movieRanking) {
+        return movieTitle + " - Director: " + movieDirector + " Rating: " + movieRanking;
+    }
+
+    public void showWelcomeMessageAndMenu() {
+        System.out.println("**********************************************************");
+        System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
+        System.out.println("**********************************************************");
+        System.out.println("*                Menu                                    *");
+        System.out.println("*         =====================                          *");
+        System.out.println("*         1. List Book Catalog                           *");
+        System.out.println("*         2. Check out Book                              *");
+        System.out.println("*         3. Check Library Number                        *");
+        System.out.println("*         4. Movie Listing                               *");
+        System.out.println("*         5. Login                                       *");
+        System.out.println("*         9. Exit                                        *");
+        System.out.println("**********************************************************");
+
+    }
+
+    public void showBookCatalogMenu() {
+        System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
+        System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
+        System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
+        System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
+
+    }
+}
